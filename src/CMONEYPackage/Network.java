b7a1da152baaca16b469897967e0080e9886ee10
cristial <cristial@mcmaster.ca>
2019-03-07 13:11:07 -0500

    added sigmoid function

--- src/CMONEYPackage/Network.java
+++ src/CMONEYPackage/Network.java
@@ -1,8 +1,21 @@
+package CMONEYPackage;
+
+import java.lang.Math;
 
 public class Network {
-
-    public Network(int sizes){
-
+	
+	private final int numLayers;
+	private final int[] sizes;
+	
+	
+    public Network(int[] sizes){
+    	this.numLayers = sizes.length;
+    	this.sizes = sizes;
+    	
+    }
+    
+    private double sigmoid(int z) {
+    	return 1.0/(1.0 + Math.exp(-z));
     }
 
 }
