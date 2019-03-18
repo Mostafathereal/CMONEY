4d9251bf3e047c1163368570329ec3e8b822319d
Mostafathereal <mohsem1@mcmaster.ca>
2019-03-17 23:01:23 -0400

    neural net

--- src/CMONEYPackage/NeuralNet.java
+++ src/CMONEYPackage/NeuralNet.java
@@ -4,12 +4,20 @@ import org.ejml.simple.SimpleMatrix;
 
 public class NeuralNet {
 
-	public static SimpleMatrix matrix = new SimpleMatrix(4, 4);
+	//public static SimpleMatrix matrix = new SimpleMatrix(4, 4);
+	private final static int[] sizes = new int[]{784, 16, 16, 10};   // manually setting the size of each layer of nodes
+	private final static int numLayers = sizes.length;
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+						
+		for(int i = 0; i < 4; i++) {
+			System.out.print(sizes[i] + "  ");
+		}
 		
-		matrix.print();
+		//matrix.print();
 
 	}
 
