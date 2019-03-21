49788ec97a718ee1ecb219af610ffada742de13d
Mostafathereal <mohsem1@mcmaster.ca>
2019-03-20 22:49:26 -0400

    setting up weights

--- src/CMONEYPackage/NeuralNet.java
+++ src/CMONEYPackage/NeuralNet.java
@@ -2,6 +2,9 @@ package CMONEYPackage;
 
 import org.ejml.simple.SimpleMatrix;
 
+import java.util.Random;
+
+
 
 public class NeuralNet {
 
@@ -9,13 +12,18 @@ public class NeuralNet {
 	private final static int[] sizes = new int[]{784, 16, 16, 10};   // manually setting the size of each layer of nodes
 	private final static int numLayers = sizes.length;
 	
+	static Random rand = new Random();
+	static SimpleMatrix A = SimpleMatrix.random(20,20,-1,1,rand);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		A.print();
 						
-		for(int i = 0; i < 4; i++) {
-			System.out.print(sizes[i] + "  ");
-			
-		}
+//		for(int i = 0; i < 4; i++) {
+//			System.out.print(sizes[i] + "  ");
+//			
+//		}
 		
 		//matrix.print();
 
