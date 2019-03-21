0a19ddc076ca8a4525bd74313611f9a9dce1c248
cristial <cristial@mcmaster.ca>
2019-03-21 13:59:32 -0400

    feedForward

--- src/CMONEYPackage/feedForward.java
+++ src/CMONEYPackage/feedForward.java
@@ -1,9 +1,15 @@
 package CMONEYPackage;
 
+import org.ejml.simple.SimpleMatrix;
+//import org.ejml.equation.*;
+
 public class feedForward {
 	
-	public void feedforward(double a) {
+	public void feedforward(SimpleMatrix a) {
 		
+		for(int i=0; i<NeuralNet.weights.length; i++) {
+			a = dot(a,NeuralNet.weights[i]) + NeuralNet.biases[i];
+		}
 	}
 	
 }
