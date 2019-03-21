9bca1b7454728bef17f2c2012afa955667d08aed
Mostafathereal <mohsem1@mcmaster.ca>
2019-03-20 23:46:32 -0400

    network initialization of weights

--- src/CMONEYPackage/NeuralNet.java
+++ src/CMONEYPackage/NeuralNet.java
@@ -1,7 +1,6 @@
 package CMONEYPackage;
 
 import org.ejml.simple.SimpleMatrix;
-
 import java.util.Random;
 
 
@@ -9,23 +8,32 @@ import java.util.Random;
 public class NeuralNet {
 
 	//public static SimpleMatrix matrix = new SimpleMatrix(4, 4);
-	private final static int[] sizes = new int[]{784, 16, 16, 10};   // manually setting the size of each layer of nodes
+	private final static int[] sizes = new int[]{4, 16, 16, 10};   // manually setting the size of each layer of nodes
 	private final static int numLayers = sizes.length;
+	static SimpleMatrix[] weight;
 	
-	static Random rand = new Random();
-	static SimpleMatrix A = SimpleMatrix.random(20,20,-1,1,rand);
+	public void NeuralNet(){
+		
+		Random rand = new Random();
+		weight = new SimpleMatrix[numLayers-1];
+		
+		for(int i = 0; i < numLayers - 1; i++) {
+			weight[i] = SimpleMatrix.random(sizes[i+1],sizes[i],-1,1,rand);
+		}
+		
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		A.print();
-						
-//		for(int i = 0; i < 4; i++) {
-//			System.out.print(sizes[i] + "  ");
-//			
-//		}
+		NeuralNet n = new NeuralNet();
+		n.NeuralNet();
+		System.out.println("hello");
 		
-		//matrix.print();
+		for(int i = 0; i < 3; i++) {
+			n.weight[i].print();
+		}
+		
 
 	}
 
