c7b8ab0f2e05eb504c1e5e62a90017332dfbaa66
cristial <cristial@mcmaster.ca>
2019-03-29 21:35:29 -0400

    finished the feedforward java

--- src/CMONEYPackage/feedForward.java
+++ src/CMONEYPackage/feedForward.java
@@ -5,15 +5,19 @@ import org.ejml.simple.SimpleMatrix;
 
 public class feedForward {
 	
-	public void feedforward(SimpleMatrix a) {
+	public SimpleMatrix feedforward(SimpleMatrix a) {
 		
 		for(int i=0; i<NeuralNet.weights.length; i++) {
 			SimpleMatrix dot = dot(a,NeuralNet.weights[i]);
 			SimpleMatrix added =  dot.plus(NeuralNet.biases[i]);
-			a = Sigmoid.sigmoidFunc(added);
-			return a;
-			
+			for(int j=0; j<a.numRows(); j++) {
+				for(int k=0; k<a.numCols(); k++) {
+					double b = added.get(j,k);
+					a.set(j, k, Sigmoid.sigmoidFunc(b));
+				}
+			}
 		}
+		return a;
 	}
 	
 	public SimpleMatrix dot(SimpleMatrix A, SimpleMatrix B) {
