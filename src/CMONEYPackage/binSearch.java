cde9efdf64e418af6cab36bd4b19277569c28041
Miles Jackson <miles@Miless-MacBook-Pro.local>
2019-04-13 15:15:09 -0400

    minor changes to binSearch

--- src/CMONEYPackage/binSearch.java
+++ src/CMONEYPackage/binSearch.java
@@ -3,7 +3,8 @@ package CMONEYPackage;
 public class binSearch {
 	
 	/**
-	 * Uses a binary search algorithm to search an array of Strings for a input currency and returns the index of it if it is in the list.
+	 * Uses a binary search algorithm which switches to a linear search when the remaining array has less 
+	 * than 4 elements left to search an array of Strings for a input currency and returns the index of it if it is in the list.
 	 * @param currencies The list of currencies to search through.
 	 * @param curr The currencies you are trying to find in the list.
 	 * @return The index of the currency if it is there if not it return -1.
@@ -11,7 +12,7 @@ public class binSearch {
 	public static int binarySearch(String[] currencies, String curr) {
 		int low = 0;
 		int high = currencies.length;
-		while(low <= high) {
+		while((high - low) > 4) {
 			int mid = low + (high-1)/2;
 			
 			int comp = curr.compareTo(currencies[mid]);
@@ -24,6 +25,9 @@ public class binSearch {
 			
 			else
 				high = mid - 1;
+		}
+		for (int i = low; i < high; i++) {
+			if (currencies[i].compareTo(curr) == 0) return i;
 		}
 		
 		return -1;
